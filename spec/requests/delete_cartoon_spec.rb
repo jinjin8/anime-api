edc598852a196392fa4c38dbce7808363899ef24
jin camou and Nicole Santamaria <jin@>
2017-07-20 11:29:04 -0700

    add & pass delete cartoon route.

---
+++ spec/requests/delete_cartoon_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe "the delete cartoon route", :type => :request do
+  cartoon = FactoryGirl.create(:cartoon)
+
+  it 'returns a deleted status' do
+    get "/cartoons/#{cartoon.id}"
+    delete "/cartoons/#{cartoon.id}"
+    expect((response)).to have_http_status(200)
+  end
+end
